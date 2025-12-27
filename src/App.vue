d82537a3b566076456cdf96023e9cca783b2d720
baekyeeee <baekyeeee@gmail.com>
2025-12-27 14:09:07 +0800

    創建認識海龜頁面

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/guide">認識海龜</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/GuideView.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <main>
+        testtest
+    </main>
+</template>
+
+<style>
+
+</style>
