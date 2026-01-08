a9988761810fddf85e665cb70af5dc01fb191558
Grim2485 <k29267597@gmail.com>
2026-01-08 11:15:35 +0800

    style.css刪除(在vue中不用watch sass) banner在layout中移除

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,12 +1,10 @@
 <script setup>
     import MainHeader from '@/components/Header.vue';
     import MainFooter from '@/components/Footer.vue';
-    import MainBanner from '@/components/Banner.vue';
 </script>
 
 <template>
     <MainHeader />
-    <MainBanner />
     <RouterView />
     <MainFooter />
 </template>
