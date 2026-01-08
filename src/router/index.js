c019573c4e59fb96f4040cc2e898b579e28a4306
Grim2485 <k29267597@gmail.com>
2026-01-08 15:33:01 +0800

    建立notFound 404頁面，點擊logo回首頁，banner動態渲染

--- src/router/index.js
+++ src/router/index.js
@@ -75,6 +75,10 @@ const router = createRouter({
       name: 'productinfo',
       component: () => import('@/views/ProductInfoView.vue'),
     },
+    {
+      path: '/:pathMatch(.*)*',
+      component: () => import('@/views/NotFound.vue')
+    }
   ],
 })
 
