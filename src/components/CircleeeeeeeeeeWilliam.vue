e2f3a402f01d1d6f42b8b35a69fc118df009f611
wclwilliam <williamlee912@gmail.com>
2026-01-10 13:43:54 +0800

    更新about us 專家

---
+++ src/components/CircleeeeeeeeeeWilliam.vue
@@ -0,0 +1,12 @@
+<template>
+  <button>圓按鈕</button>
+</template>
+<style lang="scss" scoped>
+button {
+  border-radius: 50px;
+  color: aqua;
+  background-color: #f0a;
+  font-size: 88px;
+}
+</style>
+<script></script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,29 +1,74 @@
 <script setup>
 // 列表
+import TeamMemberCard from '@/components/cards/TeamMemberCard.vue'
 import { ref, computed, onMounted } from 'vue'
 import axios from 'axios'
 // 1. 定義資料
-const productList = ref([])
+const expertList = ref([])
 // 2. 請求資料
 onMounted(() => {
-  axios.get('https://fakestoreapi.com/products').then((response) => {
-    // handle success
-    productList.value = response.data
-    //把請求的到的資料丟進productList
+  axios.get('/data/experts.json').then((response) => {
+    expertList.value = response.data
+    resultList.value = response.data
   })
 })
+
+const searchStr = ref('')
+const colors = ref('#rrggbb')
+const fontSizeValue = ref(10)
+const resultList = ref([])
+const search = () => {
+  if (searchStr.value === '') {
+    resultList.value = expertList.value
+    return
+  }
+  resultList.value = expertList.value.filter((item) => {
+    return item.title.includes(searchStr.value)
+  })
+}
+
+const listCount = computed(() => {
+  return resultList.value.length
+})
+
+import CircleeeeeeeeeeWilliam from '@/components/CircleeeeeeeeeeWilliam.vue'
 </script>
 <template>
-  <!-- {{ productList }} -->
+  <div class="container">
+    <div class="row">
+      <TeamMemberCard
+        v-for="expert in resultList"
+        :key="expert.id"
+        :image="expert.image"
+        :title="expert.title"
+        :jobTitle="expert.jobTitle"
+        :expertise="expert.expertise"
+        :description="expert.description"
+      >
+      </TeamMemberCard>
+    </div>
+  </div>
+  <!-- <h1 :style="{ fontSize: `${fontSizeValue}px` }">商品</h1>
+  <input type="number" v-model="fontSizeValue" />
+  {{ typeof fontSizeValue }}
+  <input type="text" v-model="searchStr" />
+  <input type="color" v-model="colors" />
+  {{ searchStr }}
+  {{ colors }} -->
 
-  <RouterLink :to="`/about/${product.id}`" v-for="product in productList" :key="product.id">
-    {{ product.id }}. {{ product.title }} / ${{ product.price }}
-    <div><img :src="product.image" alt="" /></div>
-  </RouterLink>
+  <h1>人員</h1>
+  <input type="text" v-model="searchStr" />
+  <button @click="search">搜尋</button>
+  <p>商品總數：{{ listCount }}</p>
+
+  <!-- <div class="color"></div> -->
+
+  <CircleeeeeeeeeeWilliam></CircleeeeeeeeeeWilliam>
 </template>
 <style scoped>
-img {
+/* .color {
   width: 100px;
-  height: auto;
-}
+  height: 100px;
+  background-color: v-bind(colors);
+} */
 </style>
