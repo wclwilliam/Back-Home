d8adb5528d8b399b885350f2533860948fa674eb
baekyeeee <baekyeeee@gmail.com>
2026-01-12 18:55:50 +0800

    海龜威脅上半部

---
+++ src/components/guide/GuideBanner.vue
@@ -0,0 +1,51 @@
+<script>
+
+</script>
+
+<template>
+ <div class="turtleBanner">
+        <div class="container">
+        <div class="turtleBannerInfo">
+            <h1 class="">你所不知道的海龜</h1>
+            <p>
+                海龜演化足跡可追溯至一億多年前的恐龍時代，是見證地球歷史的「活化石」。然而，全世界目前僅存7種海龜，如今卻全數因棲地喪失、海洋汙染及氣候變遷等衝擊，被
+                IUCN 紅色名錄列為瀕危或易危物種。這些古老的海洋旅人，正處於前所未有的生存危機之中。
+            </p>
+        </div>
+        </div>
+    </div>
+</template>
+
+<style  lang="scss" scoped>
+.turtleBanner {
+    width: 100%;
+    height: 720px;
+    background-image: url('/public/img/GuideView/turtleBanner.png');
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    align-items: center;
+}
+
+.turtleBannerInfo {
+    color: $text-white;
+    display: flex;
+    flex-direction: column;
+    margin-left: 10%;
+    width: 100%;
+    max-width: 442px;
+    gap: 86px;
+}
+
+.turtleBannerInfo h1 {
+    @include font-secondary;
+}
+
+.turtleBannerInfo p {
+    @include font-body-l;
+    //text-align: justify;
+    width: 100%;
+    max-width: 392px;
+}
+
+</style>
--- src/views/GuideView.vue
+++ src/views/GuideView.vue
@@ -1,16 +1,19 @@
 <script setup>
 import { ref, computed } from 'vue'
+import GuideBanner from '@/components/guide/GuideBanner.vue';
 import Guide from '@/components/guide/turtleGuide.vue';
 import TurtleMap from '@/components/guide/Guidemap.vue';
+import Guidethreaten from '@/components/guide/Guidethreaten.vue';
+
 
 </script>
 
 <template>
 
+    <GuideBanner></GuideBanner>
+    <Guide></Guide>
+    <TurtleMap></TurtleMap>
+    <Guidethreaten></Guidethreaten>
 
-      <Guide></Guide>
-      <TurtleMap></TurtleMap>
 
-  </template> 
-
-
+</template>
