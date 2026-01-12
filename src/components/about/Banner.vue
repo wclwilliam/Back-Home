0ad8d01f8433979004477d17d6c7a26a04a13e50
wclwilliam <williamlee912@gmail.com>
2026-01-13 00:33:14 +0800

    about進度50%

---
+++ src/components/about/Banner.vue
@@ -0,0 +1,78 @@
+<script setup>
+const parseAssets = (fileName) => {
+  return new URL(`../../assets/image/about/${fileName}`, import.meta.url).href
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="row banner_row">
+      <div class="col-sm-4 col-md-7 col-lg-7 no-gutter">
+        <div class="img_box">
+          <img :src="parseAssets('mainBanner.png')" alt="Sea Turtle" />
+        </div>
+      </div>
+
+      <div class="col-sm-4 col-md-5 col-lg-5 no-gutter">
+        <div class="bg_block"></div>
+      </div>
+
+      <div class="textCard">
+        <h1>讓每一隻受傷的靈魂，都有機會游回蔚藍。</h1>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+// 1. 處理 Grid 相關的微調
+.banner_row {
+  position: relative;
+}
+
+// 關鍵：複寫你的 Grid System padding，讓圖片跟色塊無縫接合
+.no-gutter {
+  padding-left: 0 !important;
+  padding-right: 0 !important;
+}
+
+.img_box {
+  aspect-ratio: 825.45 / 523;
+  width: 100%;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    display: block;
+  }
+}
+
+.bg_block {
+  background-color: $card-color;
+  height: 100%;
+}
+
+// 3. 浮動文字框樣式
+.textCard {
+  @include font-tertiary;
+  background-color: $secondary-color;
+  color: $text-white;
+  position: absolute;
+  z-index: 10;
+  padding: 24px 32px;
+  bottom: 40px;
+  right: 0; // 先靠右
+  margin-right: 8%;
+
+  // 手機版 RWD 調整
+  // 你的手機版是 col-sm-4 (全寬)，通常這時候不需要浮動，而是改為正常堆疊
+  @media (max-width: 767px) {
+    position: relative;
+    width: 100%;
+    bottom: auto;
+    right: auto;
+    margin-right: 0;
+    margin-top: -20px; // 稍微往上蓋住一點圖片
+  }
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <script setup>
 // 列表
-import TeamMemberCard from '@/components/cards/TeamMemberCard.vue'
+import AboutBanner from '@/components/about/Banner.vue'
+import AboutIntro from '@/components/about/Intro.vue'
+import Experts from '@/components/about/Experts.vue'
+
 import { ref, computed, onMounted } from 'vue'
 import axios from 'axios'
 // 1. 定義資料
@@ -30,22 +33,26 @@ const search = () => {
 const listCount = computed(() => {
   return resultList.value.length
 })
+
+import aboutbutton from '@/components/about/button.vue'
+
+//圖片引用方法1
+import awardImg1 from '@/assets/image/about/1.png'
+const parseAssetsIcon = (fileName) => {
+  return new URL(`../assets/image/about/${fileName}`, import.meta.url).href
+}
 </script>
 <template>
-  <div class="container">
-    <div class="row">
-      <TeamMemberCard
-        v-for="expert in resultList"
-        :key="expert.id"
-        :image="expert.image"
-        :title="expert.title"
-        :jobTitle="expert.jobTitle"
-        :expertise="expert.expertise"
-        :description="expert.description"
-      >
-      </TeamMemberCard>
-    </div>
+  <div class="banner">
+    <AboutBanner />
   </div>
+  <div class="intro">
+    <AboutIntro />
+  </div>
+  <div class="experts">
+    <Experts />
+  </div>
+
   <!-- <h1 :style="{ fontSize: `${fontSizeValue}px` }">商品</h1>
   <input type="number" v-model="fontSizeValue" />
   {{ typeof fontSizeValue }}
@@ -60,6 +67,15 @@ const listCount = computed(() => {
   <p>商品總數：{{ listCount }}</p>
 
   <!-- <div class="color"></div> -->
+  <!-- <aboutbutton title="12345"></aboutbutton>
+  <aboutbutton title="3456789"></aboutbutton>
+  <aboutbutton title="1234567" class="large"></aboutbutton> -->
+  <aboutbutton>123453456765432</aboutbutton>
+  <aboutbutton>123453456765432</aboutbutton>
+
+  <img :src="awardImg1" alt="" class="award" />
+  <!-- 圖片引用方法1 -->
+  <img :src="parseAssetsIcon('3.png')" alt="" class="award" />
 </template>
 <style scoped>
 /* .color {
@@ -67,4 +83,14 @@ const listCount = computed(() => {
   height: 100px;
   background-color: v-bind(colors);
 } */
+.award {
+  width: 200px;
+}
+
+.banner {
+  margin: 5.5rem 0;
+}
+.intro {
+  margin: 3.5rem 0;
+}
 </style>
