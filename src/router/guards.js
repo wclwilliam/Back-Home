96ddac4afe487c3a5b8291f03794cc8a6eaafd1e
kaffi41134 <babyiluhaha@gmail.com>
2026-01-17 17:36:56 +0800

    修正authGuard函數中的標題設定邏輯，移除多餘的代碼

--- src/router/guards.js
+++ src/router/guards.js
@@ -2,22 +2,13 @@ import { useAuthStore } from '@/stores/auth'
 
 export function authGuard(to) {
   const auth = useAuthStore()
-<<<<<<< Updated upstream
 
   if (to.meta && to.meta.title) {
     document.title = to.meta.title + '｜龜途 Back Home'
   }
-=======
-    
-  if( to.meta && to.meta.title){
-		document.title = to.meta.title
-	}
->>>>>>> Stashed changes
 
   if (to.meta.requiresAuth && !auth.isLogin) {
     auth.openLoginModal(to.fullPath)
     return false // 阻止導航
   }
 }
-
-
