f3a247befa2b67304c1f69a157dc0ee39b9e0a57
wclwilliam <williamlee912@gmail.com>
2026-01-17 09:40:22 +0800

    新增parseFile, publicApi 兩隻檔案、修改首頁及關於我們API串接code

---
+++ src/utils/publicApi.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export const base = import.meta.env.BASE_URL
+
+export const publicApi = axios.create({
+    baseURL: base,
+    timeout: 5000
+})
+
+// 沒有攔截器，乾乾淨淨
+export const musicApi = axios.create({
+    baseURL: 'https://api.spotify.com',
+    timeout: 5000
+})
