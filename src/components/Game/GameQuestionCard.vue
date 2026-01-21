77ff35d3eb76e16307383e22a08f7d4e59dafd86
kaffi41134 <babyiluhaha@gmail.com>
2026-01-21 16:46:33 +0800

    調整遊戲問題卡片及方向守衛的樣式，優化響應式設計

--- src/components/Game/GameQuestionCard.vue
+++ src/components/Game/GameQuestionCard.vue
@@ -9,32 +9,27 @@ const emit = defineEmits(['choose'])
 </script>
 
 <template>
-  
+
   <div class="game-question-card">
     <div class="question-container">
-    <h2 class="question-title">{{ question }}</h2>
+      <h2 class="question-title">{{ question }}</h2>
 
-    <div class="options-group">
-      <button
-        v-for="option in options"
-        :key="option.id"
-        class="btn btn-outline-game btn-xl"
-        @click="emit('choose', option)"
-      >
-        {{ option.text }}
-      </button>
-    </div>
+      <div class="options-group">
+        <button v-for="option in options" :key="option.id" class="btn btn-outline-game btn-xl"
+          @click="emit('choose', option)">
+          {{ option.text }}
+        </button>
+      </div>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
 .game-question-card {
   width: 100%;
-  background: linear-gradient(
-    0deg, rgba(227, 213, 202, 1) 0%, rgba(227, 213, 202, 0.9) 20%, rgba(227, 213, 202, 0.8) 40%, rgba(227, 213, 202, 0.7) 60%,rgba(227, 213, 202, 0.6) 70%, rgba(227, 213, 202, 0.5) 80%, rgba(227, 213, 202, 0) 100%
-  );
+  background: linear-gradient(0deg, rgba(227, 213, 202, 1) 0%, rgba(227, 213, 202, 0.9) 20%, rgba(227, 213, 202, 0.8) 40%, rgba(227, 213, 202, 0.7) 60%, rgba(227, 213, 202, 0.6) 70%, rgba(227, 213, 202, 0.5) 80%, rgba(227, 213, 202, 0) 100%);
 }
-.question-container{
+
+.question-container {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -44,8 +39,9 @@ const emit = defineEmits(['choose'])
   margin: 0 auto;
   z-index: 100;
 }
+
 .question-title {
-  @include font-body-l-bold; 
+  @include font-body-l-bold;
   flex: 1;
   min-width: 0;
 }
