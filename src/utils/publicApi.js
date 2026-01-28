bbce3131b886f5035ab854e05f5e88c15b86a0e8
wclwilliam <williamlee912@gmail.com>
2026-01-28 15:16:08 +0800

    刪除用不到的testapi

--- src/utils/publicApi.js
+++ src/utils/publicApi.js
@@ -8,22 +8,6 @@ export const publicApi = axios.create({
   timeout: 5000,
 })
 
-// 沒有攔截器，乾乾淨淨
-export const testApi = axios.create({
-  baseURL: 'https://notes.webmix.cc/ajax/teach/api',
-  timeout: 5000,
-})
-//上課測試用
-export const testApi2 = axios.create({
-  baseURL: 'https://tibamef2e.com/cjd101/g1/api',
-  timeout: 5000,
-})
-//上課測試用2
-export const test2Api = axios.create({
-  baseURL: 'https://tibamef2e.com/cjd101/g2/php/activities/list-latest.php',
-  timeout: 5000,
-})
-
 export const backHomeApi = axios.create({
   baseURL: APIBase,
   timeout: 5000,
