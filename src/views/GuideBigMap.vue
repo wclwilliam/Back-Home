8f17aa94d83f9b2bdf0c42ea422e69d85597daab
baekyeeee <baekyeeee@gmail.com>
2026-02-05 15:14:49 +0800

    修改地圖按鈕大小

--- src/views/GuideBigMap.vue
+++ src/views/GuideBigMap.vue
@@ -5,27 +5,35 @@ import turtleMap from '@/components/guide/Guidemap.vue'
 const router = useRouter()
 
 const goBack = () => {
-    router.back() 
+    router.back()
 }
 </script>
 
 <template>
     <button class="btn btn-outline" @click="goBack">上一頁</button>
-    <turtleMap/>
+    <turtleMap />
     <div class="space"></div>
 </template>
 
 <style lang="scss" scoped>
-    .btn{
-        margin-left: 50px;
-        margin-top: 50px;
-    }
-    .space{
-        margin-top: 100px;
-        @media (max-width:992px){
-             margin-top: 200px;
-            
-        }
+.btn {
+    margin-left: 50px;
+    margin-top: 50px;
+
+    @media(max-width:992px) {
+        padding: 10px 8px;
+        font-size: $m-size-caption;
+        margin-bottom: 10px;
     }
 
+}
+
+.space {
+    margin-top: 100px;
+
+    @media (max-width:992px) {
+        margin-top: 200px;
+
+    }
+}
 </style>
