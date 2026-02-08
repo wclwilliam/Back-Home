1891005ccd6cce2000539174f5d78ae810090e49
baekyeeee <baekyeeee@gmail.com>
2026-02-08 12:49:48 +0800

    修改env

--- src/components/guide/Guidethreaten.vue
+++ src/components/guide/Guidethreaten.vue
@@ -14,8 +14,8 @@ const stats = ref({
 
 
 const getApiUrl = () => {
-    if (import.meta.env.VITE_API_BASE_URL) {
-        return import.meta.env.VITE_API_BASE_URL;
+    if (import.meta.env.VITE_API_BASE) {
+        return import.meta.env.VITE_API_BASE;
     }
 };
 
